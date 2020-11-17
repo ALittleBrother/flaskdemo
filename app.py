1c1fdbe8c511127f52da248f12b7e5b02076ca9c
wuguoxing <1034370502@qq.com>
2020-11-17 20:39:34 +0800

    update trigger

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST", "OPTIONS"])
 def hello():
-    return "欢迎使用 CODING 代码模板"
+    if request.method == "POST":
+        print("触发器执行了")
+    return "欢迎使用 CODING 代码模板, 更新代码尝试自动构建"
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')    
